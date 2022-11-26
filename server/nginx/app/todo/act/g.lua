39258562bd7498f43eb3427c7c57856b40a39042
beck <2223969110@qq.com>
2022-11-26 14:41:49 +0800

    fix(server): 修正未登录时，输出数据出错

--- server/nginx/app/todo/act/g.lua
+++ server/nginx/app/todo/act/g.lua
@@ -10,19 +10,23 @@ __.name  = "To Do 全局数据"
 
 __.actx = function()
 
+
     -- 检查已登录用户
     local res  = api.pv.check_login()
     local user = res and res.user
 
     -- 取得基础数据
-    local dd = api.dd.load(user)
+    local dd
+    if user.user_id then
+       dd = api.dd.load(user)
+    end
 
     local G = {
         server_date = ngx.today(),
         server_time = ngx.now() * 1000,
         user        = user,
-        tasks       = dd.tasks,
-        task_cates  = dd.task_cates
+        tasks       = dd and dd.tasks or {},
+        task_cates  = dd and dd.task_cates or {}
     }
 
     ngx.header['Content-Type' ] = "application/javascript;charset=utf-8"
